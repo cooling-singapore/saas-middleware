e25bbe9652b2292a8c957de48e01cde50dbc208f
HeikoAydt <heiko.aydt@gmail.com>
2020-12-10 14:57:19 +0800

    Refactor/streamline blueprint helper functions

--- saas/utilities/blueprint_helpers.py
+++ saas/utilities/blueprint_helpers.py
@@ -18,36 +18,34 @@ from saas.eckeypair import ECKeyPair
 logger = logging.getLogger('Utilities.blueprint_helpers')
 
 
-def verify_request_contents(body, specification, files, required_files=None):
-    # for each element in the specification, verify the body content with the corresponding schema
-    for key in specification:
-        # check if the body has the required content
-        if key not in body:
-            return False, 400, "Missing content: '{}' required but not found.".format(key)
+class RequestError(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
 
-        # check if the content matches the schema
-        content = body[key]
-        schema = specification[key]
-        try:
-            validate(instance=content, schema=schema)
 
-        except ValidationError:
-            return False, 400, "Malformed content '{}':\ncontent={}\nschema={}".format(key, content, schema)
+class AuthenticationFailedError(RequestError):
+    def __init__(self, code, message):
+        super().__init__(code, message)
 
-    # check if all required files are available
-    if required_files:
-        for key in required_files:
-            if key not in files:
-                return False, 400, "Missing content: file '{}' required but not found.".format(key)
 
-    return True, None, None
+class AuthorisationFailedError(RequestError):
+    def __init__(self, code, message):
+        super().__init__(code, message)
+
+
+class MalformedRequestError(RequestError):
+    def __init__(self, code, message):
+        super().__init__(code, message)
 
 
 def verify_request_authentication(url, request_content):
-    # get the request body and the authentication
-    body = json.loads(request_content.form['body'])
-    authentication = json.loads(request_content.form['authentication'])
-
+    """
+    Convenient function that verifies the authentication for a request.
+    :param url: the endpoint address of the request
+    :param request_content: the request object (containing 'forms' and 'files')
+    :return:
+    """
     # process files (if any)
     files = {}
     for k in request_content.files:
@@ -57,23 +55,107 @@ def verify_request_authentication(url, request_content):
         file.save(path)
         files[k] = path
 
-    # very the the request using the provided public key
+    # check if authentication is available
+    form = request_content.form.to_dict()
+    if 'authentication' not in form:
+        raise AuthenticationFailedError(401, "Missing authentication information. Authentication failed.")
+
+    # verify the the request using the provided public key
+    authentication = json.loads(form['authentication'])
     auth_key = ECKeyPair.from_public_key_string(authentication['public_key'])
-    return auth_key.verify_authentication_token(authentication['signature'], url, body, files), body, files
+    body = json.loads(form['body']) if 'body' in form else None
+
+    if not auth_key.verify_authentication_token(authentication['signature'], url, body, files):
+        raise AuthenticationFailedError(401, "Mismatching signature. Authentication failed.")
+
+    return body, files
 
 
-def verify_request(url, request_content, body_specification={}, required_files=None):
-    # verify the request signature
-    verified, body, files = verify_request_authentication(url, request_content)
-    if not verified:
-        return False, 401, "Authentication unsuccessful.", None, None
+def verify_request_body(body, body_specification):
+    """
+    Convenient function to verify that the body of a request is adhering to its specification (i.e., JSON schema)
+    :param body: the body of request (i.e., a JSON object)
+    :param body_specification: the specification of the body (i.e., a JSON schema)
+    :return: None
+    """
+    try:
+        validate(instance=body, schema=body_specification)
 
-    # verify the request contents
-    verified, status_code, message = verify_request_contents(body, body_specification, files, required_files)
-    if not verified:
-        return False, status_code, message, None, None
+    except ValidationError:
+        raise MalformedRequestError(400, "Malformed content:\ncontent={}\nschema={}".format(body, body_specification))
 
-    return True, None, None, body, files
+
+def verify_request_files(files, required):
+    """
+    Convenient function that checks if the files of a request contain the required list of files.
+    :param files: a list of files that came with a request
+    :param required: list of required files (by filename)
+    :return:
+    """
+    # check if all required files are available
+    for key in required:
+        if key not in files:
+            raise MalformedRequestError(400, "Missing content: file '{}' required but not found.".format(key))
+
+
+def verify_authorisation_by_owner(request_content, obj_id, node, url, body=None):
+    """
+    Convenient function to verify that the authorisation for accessing a data object is valid and created by the
+    owner of the data object.
+    :param request_content: the request object (containing 'forms' and 'files')
+    :param obj_id: the data object id
+    :param node: the node that is handling the request
+    :param url: the endpoint address of the request
+    :param body: the body (if any) that came with the request
+    :return: the owner identity of the data object (if verification successful)
+    """
+    owner = node.dor.get_owner(obj_id)
+    if not owner:
+        raise AuthorisationFailedError(404, "Owner for data object '{}' not found.".format(obj_id))
+
+    # check if authentication is available
+    form = request_content.form.to_dict()
+    if 'authentication' not in form:
+        raise AuthenticationFailedError(401, "Authentication missing.")
+
+    # verify the the request using the owner public key
+    authorisation = json.loads(form['authorisation'])
+    if not owner.verify_authorisation_token(authorisation['signature'], url, body):
+        raise AuthorisationFailedError(401, "Authorisation failed.")
+
+    return owner
+
+
+def verify_authorisation_by_user(request_content, obj_id, node, url, body=None):
+    """
+    Convenient function to verify that the authorisation for accessing a data object is valid and created by a
+    permitted user of the data object.
+    :param request_content: the request object (containing 'forms' and 'files')
+    :param obj_id: the data object id
+    :param node: the node that is handling the request
+    :param url: the endpoint address of the request
+    :param body: the body (if any) that came with the request
+    :return: the user identity of the data object (if verification successful)
+    """
+    # check if authorisation is available
+    form = request_content.form.to_dict()
+    if 'authorisation' not in form:
+        raise AuthenticationFailedError(401, "Authorisation missing.")
+
+    # get the public key of the user
+    authorisation = json.loads(form['authorisation'])
+    user = ECKeyPair.from_public_key_string(authorisation['public_key'])
+
+    # does the user have access rights?
+    if not node.dor.has_access(obj_id, user):
+        raise AuthorisationFailedError(401, "User '{}' has no permission to access data object '{}'.".format(user.iid,
+                                                                                                             obj_id))
+
+    # verify the the request using the user public key
+    if not user.verify_authorisation_token(authorisation['signature'], url, body):
+        raise AuthorisationFailedError(401, "Authorisation failed.")
+
+    return user
 
 
 def create_signed_response(node, url, status_code, reply):
