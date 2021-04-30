e6b2521d5a760d51e7e8ed8a9f8a4a2ebbf2997d
heiko <heiko.aydt@gmail.com>
2021-04-30 11:20:40 +0800

    Refactor registry service

--- saas/registry/blueprint.py
+++ saas/registry/blueprint.py
@@ -1,46 +1,53 @@
-"""
-Flask blueprint for the SaaS Data Object Repository.
-"""
-
-__author__ = "Heiko Aydt"
-__email__ = "heiko.aydt@gmail.com"
-__status__ = "development"
-
 import logging
+
+from saas.rest.proxy import EndpointProxy
+from saas.utilities.blueprint_helpers import request_manager
 
 from flask import Blueprint, jsonify
 from flask_cors import CORS
 
-from saas.utilities.blueprint_helpers import request_manager
-from saas.node import Node
 
-# create the blueprint object and allows CORS for the processor route
-blueprint = Blueprint('registry', __name__)
-CORS(blueprint)
-
-logger = logging.getLogger('Registry.Blueprint')
-node: Node
+logger = logging.getLogger('registry.blueprint')
+endpoint_prefix = "/api/v1/registry"
 
 
-def initialise(node_instance):
-    global node
-    node = node_instance
+class RegistryBlueprint:
+    def __init__(self, node):
+        self._node = node
+
+    def blueprint(self):
+        # create the blueprint and allow CORS for the processor route
+        blueprint = Blueprint('registry', __name__, url_prefix=endpoint_prefix)
+        blueprint.add_url_rule('/node', self.get_node_info.__name__, self.get_node_info, methods=['GET'])
+        blueprint.add_url_rule('', self.get_registry_contents.__name__, self.get_registry_contents, methods=['GET'])
+        CORS(blueprint)
+        return blueprint
+
+    @request_manager.authentication_required
+    def get_node_info(self):
+        return jsonify({
+            "iid": self._node.id(),
+            "public_key": self._node.identity().public_as_string(),
+            "rest_service_address": self._node.rest.address(),
+            "p2p_service_address": self._node.p2p.address()
+        }), 200
+
+    @request_manager.authentication_required
+    def get_registry_contents(self):
+        return jsonify({
+            "contents": self._node.registry.get()
+        }), 200
 
 
-@blueprint.route('/node', methods=['GET'])
-@request_manager.authentication_required
-def get_node_info():
-    return jsonify({
-        "iid": node.key.iid,
-        "public_key": node.key.public_as_string(),
-        "rest_address": node.rest_api_address,
-        "p2p_address": node.server_address
-    }), 200
+class RegistryProxy(EndpointProxy):
+    def __init__(self, remote_address, sender):
+        EndpointProxy.__init__(self, endpoint_prefix, remote_address, sender)
 
+    def get_node_info(self):
+        r = self.get("/node")
+        return r['reply']
 
-@blueprint.route('', methods=['GET'])
-@request_manager.authentication_required
-def get_registry_contents():
-    return jsonify({
-        "contents": node.registry.get()
-    }), 200
+    def get_registry_contents(self):
+        r = self.get("")
+        return r['reply']['contents']
+
