5edb661098139215252607e731132ef167242bea
heiko <heiko.aydt@gmail.com>
2021-06-04 11:06:44 +0800

    Update schemas.py
    - Remove processor schema: obsolete
    - Remove workflow related schema (temporarily)
    - Modify various schemas to enable support for security concept
    - Remove data_format limitation

--- saas/schemas.py
+++ saas/schemas.py
@@ -11,7 +11,8 @@ input_interface_schema = {
         },
         'then': {
             'properties': {
-                'obj_id': {'type': 'string'}
+                'obj_id': {'type': 'string'},
+                'access_token': {'type': 'string'}
             }
         },
         'else': {
@@ -19,16 +20,22 @@ input_interface_schema = {
                 'value': {'type': 'object'}
             }
         },
+        'required': ['name', 'type']
     }
 }
 
 output_interface_schema = {
-    'type': 'object',
-    'properties': {
-        'owner_public_key': {'type': 'string'}
-        # 'visibility': {'type': 'string', 'enum': ['private', 'domain', 'public']},
-    },
-    'required': ['owner_public_key']
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'owner_public_key': {'type': 'string'},
+            'restricted_access': {'type': 'boolean'},
+            'content_encrypted': {'type': 'boolean'}
+        },
+        'required': ['name', 'owner_public_key', 'restricted_access', 'content_encrypted']
+    }
 }
 
 task_descriptor_schema = {
@@ -36,42 +43,43 @@ task_descriptor_schema = {
     'properties': {
         'processor_id': {'type': 'string'},
         'input': input_interface_schema,
-        'output': output_interface_schema
+        'output': output_interface_schema,
+        'user_public_key': {'type': 'string'}
     },
-    'required': ['processor_id', 'input', 'output']
+    'required': ['processor_id', 'input', 'output', 'user_public_key']
 }
 
-wf_task_descriptor_schema = {
-    'type': 'object',
-    'properties': {
-        'name': {'type': 'string'},
-        'processor_id': {'type': 'string'},
-        'input': input_interface_schema,
-        'output': output_interface_schema
-    },
-    'required': ['name', 'processor_id', 'input', 'output']
-}
+# wf_task_descriptor_schema = {
+#     'type': 'object',
+#     'properties': {
+#         'name': {'type': 'string'},
+#         'processor_id': {'type': 'string'},
+#         'input': input_interface_schema,
+#         'output': output_interface_schema
+#     },
+#     'required': ['name', 'processor_id', 'input', 'output']
+# }
 
-io_mapping_schema = {
-    'type': 'object',
-    'properties': {
-        'from':  {'type': 'string'},
-        'to': {'type': 'string'}
-    },
-    'required': ['from', 'to']
-}
+# io_mapping_schema = {
+#     'type': 'object',
+#     'properties': {
+#         'from':  {'type': 'string'},
+#         'to': {'type': 'string'}
+#     },
+#     'required': ['from', 'to']
+# }
 
-workflow_descriptor_schema = {
-    'type': 'object',
-    'properties': {
-        'name': {'type': 'string'},
-        'tasks': {
-            'type': 'array',
-            'items': wf_task_descriptor_schema
-        }
-    },
-    'required': ['name', 'tasks']
-}
+# workflow_descriptor_schema = {
+#     'type': 'object',
+#     'properties': {
+#         'name': {'type': 'string'},
+#         'tasks': {
+#             'type': 'array',
+#             'items': wf_task_descriptor_schema
+#         }
+#     },
+#     'required': ['name', 'tasks']
+# }
 
 data_object_descriptor_schema = {
     'type': 'object',
@@ -97,21 +105,21 @@ io_variable_schema = {
     'properties': {
         'name': {'type': 'string'},
         'data_type': {'type': 'string'},
-        'data_format': {'type': 'string', 'enum': ['json', 'hdf5']}
+        'data_format': {'type': 'string'}
     },
     'required': ['name', 'data_type', 'data_format']
 }
 
-processor_descriptor_schema = {
-    'type': 'object',
-    'properties': {
-        'created_t': {'type': 'number'},
-        'created_by': {'type': 'string'},
-    },
-    'required': ['created_t', 'created_by']
-}
+# processor_descriptor_schema = {
+#     'type': 'object',
+#     'properties': {
+#         'created_t': {'type': 'number'},
+#         'created_by': {'type': 'string'},
+#     },
+#     'required': ['created_t', 'created_by']
+# }
 
-git_specification_descriptor_schema = {
+processor_descriptor_schema = {
     'type': 'object',
     'properties': {
         'name': {'type': 'string'},
@@ -132,8 +140,8 @@ git_specification_schema = {
     'properties': {
         'source': {'type': 'string'},
         'commit_id': {'type': 'string'},
-        'path': {'type': 'string'},
-        'descriptor': git_specification_descriptor_schema
+        'proc_path': {'type': 'string'},
+        'proc_descriptor': processor_descriptor_schema
     },
-    'required': ['source', 'commit_id', 'path', 'descriptor']
+    'required': ['source', 'commit_id', 'path', 'proc_descriptor']
 }
