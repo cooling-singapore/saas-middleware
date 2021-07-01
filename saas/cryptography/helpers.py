6edfc4c28495dd0cd94a1246d85587341c962de6
heiko <heiko.aydt@gmail.com>
2021-07-01 09:23:46 +0800

    Create cryptography helpers

---
+++ saas/cryptography/helpers.py
@@ -0,0 +1,45 @@
+from cryptography.fernet import Fernet
+
+
+def symmetric_encrypt(content, key=None):
+    if key is None:
+        key = Fernet.generate_key()
+    cipher = Fernet(key)
+    return cipher.encrypt(content), key
+
+
+def symmetric_decrypt(content, key):
+    cipher = Fernet(key)
+    return cipher.decrypt(content)
+
+
+def encrypt_file(source, destination, key, chunk_size=1024*1024):
+    cipher = Fernet(key)
+    with open(destination, 'wb') as f_out:
+        with open(source, 'rb') as f_in:
+            chunk = f_in.read(chunk_size)
+            while chunk:
+                chunk = cipher.encrypt(chunk)
+                chunk_length = len(chunk)
+                length_bytes = chunk_length.to_bytes(4, byteorder='big')
+
+                f_out.write(length_bytes)
+                f_out.write(chunk)
+
+                chunk = f_in.read(chunk_size)
+
+
+def decrypt_file(source, destination, key):
+    cipher = Fernet(key)
+    with open(destination, 'wb') as f_out:
+        with open(source, 'rb') as f_in:
+            while True:
+                length_bytes = f_in.read(4)
+                if not length_bytes:
+                    break
+
+                chunk_size = int.from_bytes(length_bytes, 'big')
+                chunk = f_in.read(chunk_size)
+                chunk = cipher.decrypt(chunk)
+
+                f_out.write(chunk)
