8d1c07c857b6c4aee77deaf59a6d6cbc9fd0e941
heiko <heiko.aydt@gmail.com>
2021-09-24 12:31:19 +0800

    Address review comments

--- saas/p2p/exceptions.py
+++ saas/p2p/exceptions.py
@@ -2,69 +2,71 @@ from saas.exceptions import SaaSException
 
 
 class P2PException(SaaSException):
-    pass
+    """
+    Base exception class used for errors originating in the P2P subsystem.
+    """
 
 
 class ReceiveDataError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Problem encountered while receiving data', details=details)
+        super().__init__('Problem encountered while receiving data', details=details)
 
 
 class SendDataError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Problem encountered while sending data', details=details)
+        super().__init__('Problem encountered while sending data', details=details)
 
 
 class MalformedPreambleError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Preamble does not contain all the required elements', details=details)
+        super().__init__('Preamble does not contain all the required elements', details=details)
 
 
 class MismatchingBytesWrittenError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Unexpected number of bytes written', details=details)
+        super().__init__('Unexpected number of bytes written', details=details)
 
 
 class ResourceNotFoundError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Resource not found', details=details)
+        super().__init__('Resource not found', details=details)
 
 
 class DecodingJSONError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Problem encountered while decoding JSON object', details=details)
+        super().__init__('Problem encountered while decoding JSON object', details=details)
 
 
 class EncodingJSONError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Problem encountered while decoding JSON object', details=details)
+        super().__init__('Problem encountered while decoding JSON object', details=details)
 
 
 class HandshakeFailedError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Could not perform handshake', details=details)
+        super().__init__('Could not perform handshake', details=details)
 
 
 class MalformedMessageError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Message does not contain all the required elements', details=details)
+        super().__init__('Message does not contain all the required elements', details=details)
 
 
 class UnexpectedMessageTypeError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Unexpected message type', details=details)
+        super().__init__('Unexpected message type', details=details)
 
 
 class UnsupportedProtocolError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Protocol not supported', details=details)
+        super().__init__('Protocol not supported', details=details)
 
 
 class MismatchingRequestIdError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Request ids do not match', details=details)
+        super().__init__('Request ids do not match', details=details)
 
 
 class PeerUnavailableError(P2PException):
     def __init__(self, details: dict) -> None:
-        P2PException.__init__(self, 'Peer is not available', details=details)
+        super().__init__('Peer is not available', details=details)
