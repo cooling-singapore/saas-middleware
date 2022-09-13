98924b4d058c78e421a51b8f82a8f5fe946197a7
Reynold Mok <34395415+reyery@users.noreply.github.com>
2022-09-13 16:36:31 +0800

    Add output and process triggers

--- saas/rti/adapters/docker.py
+++ saas/rti/adapters/docker.py
@@ -1,7 +1,8 @@
-import json
 import os
 import traceback
 
+from saascore.exceptions import SaaSException
+from saascore.keystore.assets.credentials import GithubCredentials
 from saascore.log import Logging
 
 import docker
@@ -14,6 +15,9 @@ logger = Logging.get('rti.adapters.docker')
 
 
 def get_image_tag(proc_id: str) -> str:
+    """
+    Returns docker image tag as first 10 characters of proc id
+    """
     return proc_id[:10]
 
 
@@ -27,15 +31,20 @@ def prune_image(proc_id: str) -> None:
 
 
 class RTIDockerProcessorAdapter(base.RTIProcessorAdapter):
-    def __init__(self, proc_id: str, gpp: dict, obj_content_path: str, jobs_path: str, node) -> None:
+    def delete(self) -> None:
+        pass
+
+    def __init__(self, proc_id: str, gpp: dict, obj_content_path: str, jobs_path: str, node,
+                 github_credentials: GithubCredentials) -> None:
         super().__init__(proc_id, gpp, jobs_path, node)
 
-        with open(obj_content_path, 'rb') as f:
-            self._gpp = json.load(f)
+        self._gpp = gpp
+        self._github_credentials = github_credentials
 
         self.docker_image_tag = get_image_tag(proc_id)
 
     def startup(self) -> None:
+        logger.info(f"Building Docker image with tag: {self.docker_image_tag}")
         try:
             client = docker.from_env()
             client.images.build(path=os.path.join(os.path.dirname(__file__), "utilities"),
@@ -44,6 +53,7 @@ class RTIDockerProcessorAdapter(base.RTIProcessorAdapter):
                                 buildargs={"GIT_REPO": self._gpp["source"],
                                            "COMMIT_ID": self._gpp["commit_id"],
                                            "PROCESSOR_PATH": self._gpp["proc_path"],
+                                           "PROC_CONFIG": self._gpp['proc_config'],
                                            "PROC_ID": self._proc_id})
             client.close()
 
@@ -59,13 +69,27 @@ class RTIDockerProcessorAdapter(base.RTIProcessorAdapter):
     def execute(self, job_id: str, job_descriptor: dict, working_directory: str, status: StatusLogger) -> None:
         try:
             client = docker.from_env()
-
             full_working_directory = os.path.realpath(working_directory)
-            client.containers.run(self.docker_image_tag, full_working_directory,
-                                  volumes={
-                                      full_working_directory: {'bind': '/working_directory', 'mode': 'rw'}
-                                  },
-                                  remove=True)
+
+            # Detach and stream docker container output
+            container = client.containers.run(self.docker_image_tag, full_working_directory,
+                                              volumes={
+                                                  full_working_directory: {'bind': '/working_directory', 'mode': 'rw'}
+                                              },
+                                              remove=True,  # Auto remove container once done
+                                              detach=True)
+
+            # Block and go through logs until container closes
+            for log in container.logs(stream=True):
+                lines = log.decode('utf-8').splitlines()
+
+                for line in lines:
+                    if line.startswith('trigger:output'):
+                        self._handle_trigger_output(line, status, job_id, job_descriptor, working_directory)
+
+                    if line.startswith('trigger:progress'):
+                        self._handle_trigger_progress(line, status)
+
             client.close()
 
         except Exception as e:
@@ -76,3 +100,19 @@ class RTIDockerProcessorAdapter(base.RTIProcessorAdapter):
                 'working_directory': working_directory,
                 'trace': trace
             })
+
+    def _handle_trigger_output(self, line: str, status: StatusLogger, job_id: str,
+                               task_descriptor: dict, working_directory: str) -> None:
+        obj_name = line.split(':')[2]
+        try:
+            status.update(f"process_output:{obj_name}", 'push')
+            self._push_data_object(job_id, obj_name, task_descriptor, working_directory, status)
+            status.update(f"process_output:{obj_name}", 'done')
+        except SaaSException as e:
+            status.update(f"process_output:{obj_name}", f"failed: id={e.id} reason={e.reason}")
+
+    def _handle_trigger_progress(self, line: str, status: StatusLogger) -> None:
+        """
+        Line is in the format `trigger:progress:<int>`
+        """
+        status.update('progress', line.split(':')[2])
--- saas/rti/service.py
+++ saas/rti/service.py
@@ -133,7 +133,8 @@ class RuntimeInfrastructureService:
                 elif deployment == 'docker':
                     self._deployed_processors[proc_id] = \
                         docker_rti.RTIDockerProcessorAdapter(proc_id, meta['gpp'], content_path, self._jobs_path,
-                                                             self._node)
+                                                             self._node,
+                                                             github_credentials=github_credentials)
 
                 # start the processor
                 processor = self._deployed_processors[proc_id]
