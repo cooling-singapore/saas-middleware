898f12eb33ab199f7cc9148ccaf98290a666ae53
Reynold Mok <34395415+reyery@users.noreply.github.com>
2022-12-12 16:13:34 +0800

    Add pytest version of DOR test

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import tempfile
+
+import pytest
+
+from saas.core.keystore import Keystore
+from saas.dor.proxy import DORProxy
+from saas.nodedb.proxy import NodeDBProxy
+from tests.base_testcase import TestContext, update_keystore_from_credentials
+
+
+@pytest.fixture()
+def test_context():
+    context = TestContext()
+    context.initialise()
+    yield context
+    context.cleanup()
+
+
+@pytest.fixture(scope="session")
+def keystore():
+    with tempfile.TemporaryDirectory() as tempdir:
+        _keystore = Keystore.create(tempdir, f"keystore", f"no-email-provided", f"password")
+        update_keystore_from_credentials(_keystore)
+        yield _keystore
+
+
+@pytest.fixture()
+def node(test_context, keystore):
+    _node = test_context.get_node(keystore, enable_rest=True)
+    return _node
+
+
+@pytest.fixture()
+def dor_proxy(node):
+    proxy = DORProxy(node.rest.address())
+    return proxy
+
+
+@pytest.fixture()
+def node_db_proxy(node):
+    proxy = NodeDBProxy(node.rest.address())
+    return proxy
