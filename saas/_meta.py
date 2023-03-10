53127db24e09504ad3edbc60b7d225c848a381dd
Heiko Aydt <44887905+HeikoAydt@users.noreply.github.com>
2023-03-10 17:48:20 +0800

    Update _meta.py

--- saas/_meta.py
+++ saas/_meta.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0-alpha'
+__version__ = '0.3.1-alpha'
 __name__ = 'saas-middleware'
 __title__ = 'SaaS Middleware'
 __description__ = 'Middleware for powering digital twin federations of models'
