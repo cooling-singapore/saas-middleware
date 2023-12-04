f501c88e980f80ce651d640e2ac5e43a6457cad1
heiko <aydt@arch.ethz.ch>
2023-12-04 12:44:01 +0800

    Update version counter

--- saas/meta.py
+++ saas/meta.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.0-alpha'
+__version__ = '0.7.0-alpha'
 __name__ = 'saas-middleware'
 __title__ = 'SaaS Middleware'
 __description__ = 'Middleware for powering digital twin federations of models'
