81bef9a33390ef97b3ce0d4617ca2cfcfb376976
HeikoAydt <heiko.aydt@sec.ethz.ch>
2024-06-21 21:08:37 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ from saas.rti.proxy import RTIProxy
 from saas.rti.schemas import Processor
 from tests.base_testcase import TestContext, update_keystore_from_credentials, PortMaster
 
-commit_id = '4b4647fbac244527c27937ac3e57d08a98ef3d3a'
+commit_id = 'f1839919832b487a5b9a574a39606f867498d452'
 ssh_key_path = os.path.join(os.environ['HOME'], 'Desktop', 'OneDrive', 'operations', 'ssh', 'id_testing')
 
 
